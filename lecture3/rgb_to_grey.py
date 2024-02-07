7661cfbe3001a84f44097db9e1c25e3272869bda
cudawarped <12133430+cudawarped@users.noreply.github.com>
2024-02-07 17:28:07 +0200

    Make example clearer with pointers instead of bit shifts.  Introduce test and ensure image is reset before each one

--- lecture3/rgb_to_grey.py
+++ lecture3/rgb_to_grey.py
@@ -6,6 +6,10 @@ import matplotlib.pyplot as plt
 from torch.utils.cpp_extension import load_inline
 
 img = io.read_image('puppy.jpg')
+img_grey_gs = (0.2989*img[0,:,:] + 0.5870*img[1,:,:] + 0.1140*img[2,:,:]).type(torch.uint8)
+
+def assert_equal(img_1, img_2, atol = 1):
+    assert torch.isclose(img_1,img_2, atol=atol).all()
 
 def load_cuda(cuda_src, cpp_src, funcs, opt=False, verbose=False):
     return load_inline(cuda_sources=[cuda_src], cpp_sources=[cpp_src], functions=funcs,
@@ -39,31 +43,51 @@ __global__ void rgb_to_grayscale_kernel_int(unsigned char* x, unsigned char* out
     if (i<n) out[i] = ((77*x[i] + 150*x[i+n] + 29*x[i+2*n]) >> 8);
 }
 
+// coalesced read/write using floats for grey calc - example implementation without handler for unaligned memory or width which is not divisible by 4
 __global__ void rgb_to_grayscale_kernel_float_coalesced(unsigned char* x, unsigned char* out, int n) {
-    int i = blockIdx.x*blockDim.x + threadIdx.x;
-    if(i>n) return;
-    
+    int idx = blockIdx.x*blockDim.x + threadIdx.x;
+    if(idx>n) return;
     unsigned int *xUint = (unsigned int*)x;
-    const unsigned int rVec = xUint[i];
-    const unsigned int gVec = xUint[i+n];
-    const unsigned int bVec = xUint[i+2*n];
-    unsigned char greyVec[4];
+    const unsigned int rVec = xUint[idx];
+    const unsigned int gVec = xUint[idx+n];
+    const unsigned int bVec = xUint[idx+2*n];
+    unsigned int greyVec;
+    unsigned char* grey = (unsigned char*)&greyVec;
     for (int i = 0; i < 4; i++) {
-        const unsigned char r = ((rVec >> i * 8) & 0xFFU);
-        const unsigned char g = ((gVec >> i * 8) & 0xFFU);
-        const unsigned char b = ((bVec >> i * 8) & 0xFFU);
-        greyVec[i] = 0.2989f*r + 0.5870f*g + 0.1140f*b;
+        //const unsigned char r = ((rVec >> i * 8) & 0xFFU);
+        const unsigned char r = ((const unsigned char*)&rVec)[i];
+        const unsigned char g = ((const unsigned char*)&gVec)[i];
+        const unsigned char b = ((const unsigned char*)&bVec)[i];
+        grey[i] = 0.2989f*r + 0.5870f*g + 0.1140f*b;
     }
-
     unsigned int *outUint = (unsigned int*)out;
-    outUint[i] = *((unsigned int*)greyVec);
+    outUint[idx] = greyVec;
 }
 
-torch::Tensor rgb_to_grayscale(torch::Tensor input, int approach, bool internalTimer) {
+// block unrolled and coalesced read/write using floats for grey calc no safety checking
+__global__ void rgb_to_grayscale_kernel_float_coalesced_unroll(unsigned char* x, unsigned char* out, int n) {
+    int idx = blockIdx.x*blockDim.x + threadIdx.x;
+    unsigned int *xUint = (unsigned int*)x;
+    const unsigned int rVec = xUint[idx];
+    const unsigned int gVec = xUint[idx+n];
+    const unsigned int bVec = xUint[idx+2*n];
+    unsigned int greyVec;
+    unsigned char* grey = (unsigned char*)&greyVec;
+    for (int i = 0; i < 4; i++) {
+        //const unsigned char r = ((rVec >> i * 8) & 0xFFU);
+        const unsigned char r = ((const unsigned char*)&rVec)[i];
+        const unsigned char g = ((const unsigned char*)&gVec)[i];
+        const unsigned char b = ((const unsigned char*)&bVec)[i];
+        grey[i] = 0.2989f*r + 0.5870f*g + 0.1140f*b;
+    }
+    unsigned int *outUint = (unsigned int*)out;
+    outUint[idx] = greyVec;
+}
+
+void rgb_to_grayscale(torch::Tensor input, torch::Tensor output, int approach, bool internalTimer) {
     CHECK_INPUT(input);
     int h = input.size(1);
     int w = input.size(2);
-    auto output = torch::empty({h,w}, input.options());
     int threads = 256;
     cudaEvent_t start, end;
     if(internalTimer){ 
@@ -87,60 +111,67 @@ torch::Tensor rgb_to_grayscale(torch::Tensor input, int approach, bool internalT
     case 3:
         rgb_to_grayscale_kernel_float_coalesced<<<cdiv(w/4*h,threads), threads>>>(
             input.data_ptr<unsigned char>(), output.data_ptr<unsigned char>(), w/4*h);
-        }
+            }
     if(internalTimer){
         cudaEventRecord(end);
         cudaEventSynchronize(end);
         float ms;
         cudaEventElapsedTime(&ms, start, end);
-        printf("elTime (ns): %f\n", ms*1000);        
+        printf("elTime (ns): %.2f\n", ms*1000);
         cudaEventDestroy(start);
         cudaEventDestroy(end);
     }
-    return output;
 }'''
 
-cpp_src = "torch::Tensor rgb_to_grayscale(torch::Tensor input, int approach, bool internalTimer);"
+cpp_src = "void rgb_to_grayscale(torch::Tensor input, torch::Tensor output, int approach, bool internalTimer);"
 
 module = load_cuda(cuda_src, cpp_src, ['rgb_to_grayscale'], verbose=True)
 
 [o for o in dir(module) if o[0]!='_']
 
 imgc = img.contiguous().cuda()
+img_grey = torch.zeros((imgc.size(1),imgc.size(2)), dtype=torch.uint8).cuda()
 
 start = torch.cuda.Event(True)
 end = torch.cuda.Event(True)
-n_to_warmup = 10
-n_to_average = 10
+n_to_warmup = 5
+n_to_average = 5
 # warmup
 for i in range(n_to_warmup):
-    res = module.rgb_to_grayscale(imgc, 1, False)
+    module.rgb_to_grayscale(imgc, img_grey, 0, False)
 
-
+img_grey.zero_()
 start.record()
-for i in range(10):
-    res = module.rgb_to_grayscale(imgc, 0, False)
+for i in range(n_to_average):
+    module.rgb_to_grayscale(imgc, img_grey, 0, False)
 end.record()
 end.synchronize()
+assert_equal(img_grey_gs, img_grey.cpu())
 print(f'Kernel time (double): {start.elapsed_time(end)*1000/n_to_average:.2f}ns')
 
+img_grey.zero_()
 start.record()
 for i in range(n_to_average):
-    res = module.rgb_to_grayscale(imgc, 1, False)
+    module.rgb_to_grayscale(imgc, img_grey, 1, False)
 end.record()
 end.synchronize()
+assert_equal(img_grey_gs, img_grey.cpu())
 print(f'Kernel time (float): {start.elapsed_time(end)*1000/n_to_average:.2f}ns')
 
+img_grey.zero_()
 start.record()
 for i in range(n_to_average):
-    res = module.rgb_to_grayscale(imgc, 2, False)
+    module.rgb_to_grayscale(imgc, img_grey, 2, False)
 end.record()
 end.synchronize()
+assert_equal(img_grey_gs, img_grey.cpu())
 print(f'Kernel time (int): {start.elapsed_time(end)*1000/n_to_average:.2f}ns')
 
+img_grey.zero_()
 start.record()
 for i in range(n_to_average):
-    res = module.rgb_to_grayscale(imgc, 3, False)
+    module.rgb_to_grayscale(imgc, img_grey, 3, False)
 end.record()
 end.synchronize()
+assert_equal(img_grey_gs, img_grey.cpu())
 print(f'Kernel time (coalesced floats): {start.elapsed_time(end)*1000/n_to_average:.2f}ns')
